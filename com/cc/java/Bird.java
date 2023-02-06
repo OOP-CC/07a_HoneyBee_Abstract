eac20a0549008aa2d463fff5ac02b2f83eb1e454
Thirhag <meigraf@hotmail.de>
2023-02-06 19:24:04 +0100

    Incl_Bird_endstand

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class Bird extends HoneyBee {
+
+    @Override
+    public String doYourJobString(){
+        return "But i´m a BIRD!!!!!!!!!!!!"
+    }
+    @Override 
+    public String fly(){
+        return "I´m a BIRD, i can also fly!!";
+    }
+}
